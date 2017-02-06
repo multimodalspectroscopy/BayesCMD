603b700e81eda5abe0345a1680c71c4a18cb03b0
Josh Buckland <buck06191@gmail.com>
2017-02-06 18:21:11 +0000

    code completed to generate modelJSON files

--- BayesCMD/jsonParsing/modelJSON.py
+++ BayesCMD/jsonParsing/modelJSON.py
@@ -1,13 +1,19 @@
 import json
 import os.path
 import re
+import pprint
+import subprocess
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from bcmdModel.bcmd_model import ModelBCMD
+from io import StringIO
 
 
 def float_or_str(n):
     try:
         s = float(n)
         return s
-    except:
+    except ValueError:
         return n
 
 
@@ -16,46 +22,34 @@ def get_model_name(fpath):
     return fname.split('.')[0]
 
 
+def json_writer(model_name, dictionary):
+    with open('%s.json' % model_name, 'w') as fp:
+        json.dump(dictionary, fp)
+
+
 def modeldefParse(fpath):
-    model_data = dict()
+    model_data = {'params': {}}
 
     with open(fpath) as f:
         model_data['model_name'] = get_model_name(fpath)
         for line in filter(None, (line.rstrip() for line in f)):
-            if ':=' in line:
-                line = line.strip('\n')
-                model_data['params'][line.split(':=')[0].strip(' ')] =
-                ''.join(line.split(':=')[1].strip(' '))
             li = line.lstrip()
             li = li.split()
             if li[0][:1] == '@':
                 model_data.setdefault(li[0][1:],
                                       []).extend([item for item in li[1:]])
+    model = ModelBCMD(model_data['model_name'])
+    result = StringIO(model.get_defaults().stdout.decode())
+    model_data['params'].update({line.strip('\n').split('\t')[0]:
+                                 line.strip('\n').split('\t')[1]
+                                 for line in result})
+    json_writer(model_data['model_name'],model_data)
+
     return model_data
 
 
-def param_replacement(param_dict, expression_string):
-    """
-    Function that will go through the dictionary of parameters, find them in
-    expressions and replace with the appropriate number.
-
-    : param param_dict: Dictionary of parameters.
-    : param expression_string: String to find and replace inside.
-    """
-    pattern = re.compile(r'\b(' + '|'.join(param_dict.keys()) + r')\b')
-
-    result = pattern.sub(lambda x: str(d[x.group()]), expression_string)
-
-    return result
-
-
-def json_writer(model_name, dictionary):
-    with open('%s.json' % model_name, 'w') as fp:
-        json.dump(sample, fp)
-
 if __name__ == '__main__':
     import argparse
-    import pprint
     ap = argparse.ArgumentParser(description='Process some integers.')
     ap.add_argument('model_file', metavar='FILE', help='the modeldef file')
     args = ap.parse_args()
