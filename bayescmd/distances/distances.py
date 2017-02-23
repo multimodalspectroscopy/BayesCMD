1a761b87b4dc1b1488e3488ffca6b898445f95ab
Josh Buckland <buck06191@gmail.com>
2017-02-23 11:51:40 +0000

    make euclidean distance use class variables

--- bayescmd/distances/distances.py
+++ bayescmd/distances/distances.py
@@ -1,26 +1,29 @@
 import numpy as np
 from .data_import import *
-# All functions here can expect to handle the output from BCMD Model i.e. a dict.
+# All functions here can expect to handle the output from BCMD Model i.e.
+# a dict.
 
 
 def check_for_key(dictionary, target):
-	try:
-		data = dictionary[target]
-	except KeyError:
-		print('Actual data does not contain target value.')
-	return data
+    try:
+        data = dictionary[target]
+    except KeyError:
+        print('Actual data does not contain target value.')
+    return data
+
 
 class DistanceMeasures:
+
     def __init__(self,
-    			 target_value,
-    			 simulation_data,
-    			 actual_data_file):
-    	
-		self.actual_data = check_for_key(actual_data_import(actual_data_file), target_value)
+                 target_value,
+                 simulation_data,
+                 actual_data_file):
 
-			
-		self.sim_data = check_for_key(sim_data, target_value)
+        self.actual_data = check_for_key(
+            actual_data_import(actual_data_file), target_value)
 
+        self.sim_data = check_for_key(sim_data, target_value)
 
     def euclidean_dist():
-        return [numpy.sqrt(numpy.sum((data1 - data2) * (data1 - data2)))]
+        return [numpy.sqrt(numpy.sum((self.actual_data - self.sim_data) *
+                                     (self.actual_data - self.sim_data)))]
