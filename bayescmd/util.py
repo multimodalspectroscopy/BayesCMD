0dcff3f9bee2170ff544a7835f20411e013f5d55
Josh Buckland <buck06191@gmail.com>
2017-03-07 14:19:36 +0000

    simple utility module

---
+++ bayescmd/util.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+
+def findBaseDir(max_depth=5):
+    MAX_DEPTH = max_depth
+    BASEDIR = os.path.abspath(os.path.dirname(__file__))
+    for level in range(MAX_DEPTH):
+        print('LEVEL %d: %s' % (level, BASEDIR))
+        if os.path.basename(BASEDIR) == 'BayesCMD':
+            break
+        else:
+            BASEDIR = os.path.abspath(os.path.dirname(BASEDIR))
+        if level == MAX_DEPTH-1:
+            print('Could not find correct basedir')
+            sys.exit(status=2)
+    return os.path.relpath(BASEDIR)
