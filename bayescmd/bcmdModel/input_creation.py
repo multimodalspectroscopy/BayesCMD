2eda517f73d15910f4354ebd885bf000cd44d0ae
Josh Buckland <ucbpjru@ucl.ac.uk>
2017-06-26 14:54:07 +0100

    updated bayescmd with code from WeBCMd

--- bayescmd/bcmdModel/input_creation.py
+++ bayescmd/bcmdModel/input_creation.py
@@ -20,58 +20,62 @@ class InputCreator:
         print("Input file written to %s" % os.path.abspath(self.filename))
         return self.filename
 
-        def default_creation(self):
-            """
-            Create a default input file from given arguments. Assumes parameters
-            remain unchanged.
-            :param times: List of times, as per input data. Length should be 1 more
-            than actual number of time steps (0-base)
-            :param inputs: dictionary of 'inputs'. This is any thing, inputs or
-            params, which are defined at subsequent timepoints.
-            :return: Output string buffer
-            """
-            if self.inputs is not None:
-                assert len(self.times) == len(self.inputs['values']), "Different" \
-                    "number of time steps in log and in data:\n \t" \
-                    "time steps = %d \n\t" \
-                    "input steps = %d" % (len(self.times[:-1]),
-                                          len(self.inputs['values']))
-
-            self.f_out.write('# File created using BayesCMD file creation\n')
-            self.f_out.write('@%d\n' % len(self.times[:-1]))
-            if self.inputs is not None:
-                self.f_out.write(': %d ' % len(self.inputs['names']) +
-                                 ' '.join(self.inputs['names']) + '\n')
-                for ii, time in enumerate(self.times[:-1]):
-                    self.f_out.write('= %d %d ' % (self.times[ii],
-                                                   self.times[ii + 1]) +
-                                     ' '.join(str(v) for v in self.inputs['values'][ii]) + '\n')
-            else:
-                self.f_out.write(':0\n= -1 0\n')
-                for ii, time in enumerate(self.times[:-1]):
-                    self.f_out.write('= %f %f ' %
-                                     (self.times[ii], self.times[ii + 1]) + '\n')
-
-            self.f_out.seek(0)
-            return self.f_out
-
-    def initialised_creation(self):
+    def default_creation(self):
         """
-        Create an input file from given arguments that will initialise.
-        Assumes parameters remain unchanged.
+        Create a default input file from given arguments. Assumes parameters
+        remain unchanged.
         :param times: List of times, as per input data. Length should be 1 more
         than actual number of time steps (0-base)
         :param inputs: dictionary of 'inputs'. This is any thing, inputs or
         params, which are defined at subsequent timepoints.
-        : param params: dict of non-default parameters of form name: val to
+        :return: Output string buffer
+        """
+        if self.inputs is not None:
+            assert len(self.times) == len(self.inputs['values']), "Different" \
+                "number of time steps in log and in data:\n \t" \
+                "time steps = %d \n\t" \
+                "input steps = %d" % (len(self.times[:-1]),
+                                      len(self.inputs['values']))
+
+        self.f_out.write('# File created using BayesCMD file creation\n')
+        self.f_out.write('@%d\n' % len(self.times))
+        if self.inputs is not None:
+            self.f_out.write(': %d ' % len(self.inputs['names']) +
+                             ' '.join(self.inputs['names']) + '\n')
+            self.f_out.write('= -1 ' + str(self.times[0]) + ' ' +
+                             ' '.join(str(v) for v in
+                                      self.inputs['values'][0]) +
+                             '\n')
+            for ii in range(len(self.times[:-1])):
+                self.f_out.write('= %f %f ' % (self.times[ii],
+                                               self.times[ii + 1]) +
+                                 ' '.join(str(v) for v in self.inputs['values'][ii]) + '\n')
+        else:
+            self.f_out.write(':0\n= -1 ' + str(self.times[0]) + '\n')
+            for ii in range(len(self.times[:-1])):
+                self.f_out.write('= %f %f ' %
+                                 (self.times[ii], self.times[ii + 1]) + '\n')
+
+        self.f_out.seek(0)
+        return self.f_out
+
+    def initialised_creation(self, burn_in):
+        """
+        Create an input file from given arguments that will initialise.
+        Assumes parameters remain unchanged.
+        :param burn_in: Length of burn in
+        :param times: List of times, as per input data. Length should be 1 more
+        than actual number of time steps (0-base)
+        :param inputs: dictionary of 'inputs'. This is any thing, inputs or
+        params, which are defined at subsequent timepoints.
+        : param params: dict of non-default parameters of form {name: val} to
         initialise at start
         :param outputs: list of target outputs
         :return: Output string buffer
         """
 
         self.f_out.write('# File created using BayesCMD file creation\n')
-        self.f_out.write('@ %d\n' % (len(self.times) + 1))
-        self.f_out.write('>>> 0\n!0\n')
+
         # Create lists for initialised names and values
         init_names = []
         init_vals = []
@@ -84,11 +88,27 @@ class InputCreator:
             init_names.extend(self.inputs['names'])
             init_vals.extend(self.inputs['values'][0])
 
-        self.f_out.write(':%d ' % len(init_names) +
-                         ' '.join(init_names) +
-                         '\n')
-        self.f_out.write('= -1000 -1 ' + ' '.join(str(v) for v in init_vals) +
-                         '\n')
+        if burn_in > 0:
+            self.f_out.write('@ %d\n' % (len(self.times) + 1))
+            self.f_out.write('>>> 0\n!0\n')
+            self.f_out.write(':%d ' % len(init_names) +
+                            ' '.join(init_names) +
+                            '\n')
+
+            self.f_out.write('= -%f -1 ' % (burn_in + 1) +
+                             ' '.join(str(v) for v in init_vals) +
+                             '\n')
+        else:
+            self.f_out.write('@ %d\n' % len(self.times))
+            self.f_out.write('>>> 0\n!0\n')
+            self.f_out.write(':%d ' % len(init_names) +
+                            ' '.join(init_names) +
+                            '\n')
+
+            self.f_out.write('= -2 -1 ' +
+                             ' '.join(str(v) for v in init_vals) +
+                             '\n')
+        # Set post burn in outputs and values
         if len(self.outputs) == 0:
             self.f_out.write('>>> 1 t \n!!!\n')
         elif len(self.outputs) == 1:
@@ -114,19 +134,19 @@ class InputCreator:
             self.f_out.write(':%d ' % len(self.inputs['names']) +
                              ' '.join(self.inputs['names']) +
                              '\n')
-            self.f_out.write('= -1 0 ' +
+            self.f_out.write('= -1 %s ' % (self.times[0]) +
                              ' '.join(str(v) for v in
                                       self.inputs['values'][0]) +
                              '\n')
-            for ii, time in enumerate(self.times[:-1]):
+            for ii in range(len(self.times[:-1])):
                 self.f_out.write('= %f %f ' % (self.times[ii],
                                                self.times[ii + 1]) +
                                  ' '.join(str(v) for v in
                                           self.inputs['values'][ii + 1]) +
                                  '\n')
         else:
-            self.f_out.write(':0\n= -1 0\n')
-            for ii, time in enumerate(self.times[:-1]):
+            self.f_out.write(':0\n= -1 %f\n' % (self.times[0]))
+            for ii in range(len(self.times[:-1])):
                 self.f_out.write('= %f %f ' %
                                  (self.times[ii], self.times[ii + 1]) + '\n')
 
