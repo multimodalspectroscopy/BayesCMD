d3b6b033bf7c677e50e02d42f73862fc642f5e62
Josh Buckland <ucbpjru@ucl.ac.uk>
2017-09-19 10:12:40 +0100

    fixed error in how parametrs were handled in shell script

--- batch/optimisation_compilation.sh
+++ batch/optimisation_compilation.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 # A file to create optimisation logs and then extract the param values and add to a JSON
-# Written by: Joshua Russell-Buckland 
+# Written by: Joshua Russell-Buckland
 # Last updated on: 1/8/2017
 
 
-usage="$(basename "$0") [-h] [-n MAX_N] -- program to run optimisation code multiple times and compile results into a JSON
+usage="$(basename "$0") [-h] [-N number of parameters][-n MAX_N] -- program to run optimisation code multiple times and compile results into a JSON
 
 where:
     -h  show this help text
+    -N  Number of parameters being optimised
     -n  set the max number of runs (Default: 10)"
 
 MAX_N=10
-while getopts ':hn:' option; do
+pflag=false
+while getopts ':hN:n:' option; do
   case "$option" in
     h) echo "$usage"
        exit
+       ;;
+    N) pflag=true; Nparams=$OPTARG
        ;;
     n) MAX_N=$OPTARG
        ;;
@@ -30,18 +34,26 @@ while getopts ':hn:' option; do
 done
 shift $((OPTIND - 1))
 
+if ! $pflag
+then
+  echo "Number of parameters not provided."
+  exit 1
+fi
 
-printf "{\n" > ../data/all_opt.json
+DATE=`date '+%d%m%yT%H:%M:%S'`
+mkdir "../data/$DATE"
+echo "Created directory: ../data/$DATE"
+printf "{\n" > ../data/$DATE/all_opt.json
 
 echo "Running $MAX_N times"
 for N in $(seq 0 $MAX_N)
 do
   echo "Processing run $N"
-  python -u optim.py optjob_files/hx01_BS.optjob ../data/hx01.csv > >(tee -a ../data/optimisation_$N.log)
+  python -u optim.py optjob_files/hx01_BS.optjob ../data/hx01.csv > >(tee -a ../data/$DATE/optimisation_$N.log)
   printf "\"$N\":" >> ../data/all_opt.json
-  python optimisation_compile.py ../data/optimisation_$N.log >>  ../data/all_opt.json
+  python optimisation_compile.py ../data/optimisation_$N.log $Nparams >>  ../data/$DATE/all_opt.json
 done
-truncate -s-2 ../data/all_opt.json
-printf "}" >> ../data/all_opt.json
+truncate -s-2 ../data/$DATE/all_opt.json
+printf "}" >> ../data/$DATE/all_opt.json
 
-more ../data/all_opt.json
+more ../data/$DATE/all_opt.json
--- batch/optimisation_compile.py
+++ batch/optimisation_compile.py
@@ -15,17 +15,18 @@ def tail(filename, n):
         s[0] = '"'+s[0]+'"'
         s = ":".join(s)
         return s
-         
+
     while True:
         if p.poll(1):
             return "{" + ",".join([_byte_converter(x) for x in f.stdout.readlines()])+"},"
 
 
 if __name__=="__main__":
-    
+
     parser = argparse.ArgumentParser("Pass in file to be converted to JSON and printed")
     parser.add_argument('filename', help="file to be processed")
-    
-    args=parser.parse_args()
-    
-    print(tail(args.filename,5))
+    parser.add_argument('n_params', help="Number of parameters being optimised")
+
+    args = parser.parse_args()
+
+    print(tail(args.filename, args.n_params))
