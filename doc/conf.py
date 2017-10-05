d1d2929c7304f136e3e61066552cf54262706559
buck06191 <buck06191@gmail.com>
2017-10-05 14:11:13 +0100

    try fix build error

--- doc/conf.py
+++ doc/conf.py
@@ -24,7 +24,7 @@ import mock
 sys.path.insert(0, os.path.abspath('..'))
 
 MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot',
-                'scipy.interpolate']
+                'numpy.random']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
