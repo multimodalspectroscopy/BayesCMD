8d84083995dc0542940a89e7deeaeb976408430f
buck06191 <buck06191@gmail.com>
2017-10-12 11:51:23 +0100

    moved Batch creation to ABC sub[ackage

--- bayescmd/abc/__init__.py
+++ bayescmd/abc/__init__.py
@@ -1,4 +1,5 @@
 from .rejection import Rejection
+from .batch_data_creation import Batch
 from .distances import get_distance
 from .data_import import import_actual_data, inputParse
 from .params_creator import priors_creator
