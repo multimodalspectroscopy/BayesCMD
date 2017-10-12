646d2c31733e5dbaeac458429b0d80a8d28a5d1e
buck06191 <buck06191@gmail.com>
2017-10-12 11:52:19 +0100

    modified hyperapnia_batch to allow for new location of Batch module

--- hypercapnia_batch.py
+++ hypercapnia_batch.py
@@ -1,7 +1,8 @@
-from batch_data_creation import Batch
+"""Run a simple hypercapnia batch using the BS model."""
+from bayescmd.abc import Batch
 from datetime import datetime
 import os
-import sys
+# import sys
 import pprint
 import distutils
 import argparse
@@ -12,35 +13,35 @@ BASEDIR = findBaseDir('BayesCMD')
 model_name = 'BS'
 inputs = ['Pa_CO2', 'P_a', 'SaO2sup']  # Input variables
 
-priors = priors_creator({"Vol_mit": 0.067,
-                         "r_t": 0.018,
-                         "r_m": 0.027,
-                         "r_0": 0.0126,
-                         "O2_n": 0.024,
-                         "cytox_tot_tis": 0.0055,
-                         "v_cn": 40,
-                         "sigma_coll": 62.79}, 0.25)
+priors = priors_creator({
+    "Vol_mit": 0.067,
+    "r_t": 0.018,
+    "r_m": 0.027,
+    "r_0": 0.0126,
+    "O2_n": 0.024,
+    "cytox_tot_tis": 0.0055,
+    "v_cn": 40,
+    "sigma_coll": 62.79
+}, 0.25)
 outputs = ['Vmca', 'CCO']
 
 
 def process(run_length, input_file, workdir):
-    batchWriter = Batch(model_name,
-                        priors,
-                        inputs,
-                        outputs,
-                        run_length,
-                        input_file,
-                        workdir)
+    batchWriter = Batch(model_name, priors, inputs, outputs, run_length,
+                        input_file, workdir, store_simulations=False)
 
     batchWriter.definePriors()
     batchWriter.batchCreation(zero_flag={k: False for k in outputs})
 
+
 if __name__ == '__main__':
     ap = argparse.ArgumentParser('Choose batch run data and length:')
-    ap.add_argument('input_file', metavar="INPUT_FILE",
-                    help='batch data')
-    ap.add_argument('run_length', metavar='RUN_LENGTH', type=int,
-                    help='number of times to run the model')
+    ap.add_argument('input_file', metavar="INPUT_FILE", help='batch data')
+    ap.add_argument(
+        'run_length',
+        metavar='RUN_LENGTH',
+        type=int,
+        help='number of times to run the model')
 
     args = ap.parse_args()
     now = datetime.now().strftime('%H%MT%d%m%y')
