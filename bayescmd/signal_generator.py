28c7886fcb4b86f4d44082e3b428ee7707c737c1
buck06191 <buck06191@gmail.com>
2018-04-30 17:42:37 +0100

    Signal generator includes single step function

--- bayescmd/signal_generator.py
+++ bayescmd/signal_generator.py
@@ -37,7 +37,7 @@ def rescale(vv, lo=0, hi=1):
     return scaled_sig
 
 
-def wave(tt, lo=0, hi=1, freq=0.1, phi=0, kind='saw', **kwargs):
+def wave(tt, lo=0, hi=1, freq=0.1, sample_freq=1, phi=0, kind='saw', **kwargs):
     """
     Generate a single test waveform at specified time points.
 
@@ -53,7 +53,9 @@ def wave(tt, lo=0, hi=1, freq=0.1, phi=0, kind='saw', **kwargs):
     hi : :obj:`float`, optional
         Highest value to scale to, if scaling. Default is 1.
     freq : :obj:`float`, optional
-        Frequency of the signal. Default is 0.1Hz
+        Frequency of the signal. Default is 0.1Hz.
+    sample_freq : :obj:`float`, optional
+        Sampling frequency of the time series. Default is 1.
     phi : :obj:`float`, optional
         Phase shift for rescaling time points. Default is 0.
     kind : :obj:`str`, optional
@@ -64,8 +66,10 @@ def wave(tt, lo=0, hi=1, freq=0.1, phi=0, kind='saw', **kwargs):
             "uniform" : uniform wave
             "gaussian" : normally distributed random numbers
             "walk" : random walk
+            "tophat" : single step function
         Each wave takes keyword arguments that can be passed through the
-        function.
+        function. For "tophat", the start and end are taken to be 25 and 75%
+        of total end time respectively.
         Default wave is "saw".
 
     Returns
@@ -76,7 +80,7 @@ def wave(tt, lo=0, hi=1, freq=0.1, phi=0, kind='saw', **kwargs):
     """
     # periodic signal functions have defined period 2 * pi, so we
     # scale the time points accordingly
-    sct = tt * freq * 2 * np.pi + phi
+    sct = (tt/sample_freq) * freq * 2 * np.pi + phi
 
     if kind == 'saw':
         if kwargs is not None and 'width' in kwargs:
@@ -102,6 +106,19 @@ def wave(tt, lo=0, hi=1, freq=0.1, phi=0, kind='saw', **kwargs):
         if kwargs is not None:
             ss = ss * kwargs.get('sd', 1) + kwargs.get('mean', 0)
         ss = np.cumsum(ss)
+    elif kind == 'tophat':
+        if kwargs is not None and 'start' in kwargs:
+            start = kwargs['start']
+        else:
+            start = 0.25
+
+        if kwargs is not None and 'end' in kwargs:
+            end = kwargs['end']
+        else:
+            end = 0.75
+
+        ss = tt * 0
+        ss[int(len(tt) * start):int(len(ss) * end)] = 1
 
     # potentially other kinds here
 
