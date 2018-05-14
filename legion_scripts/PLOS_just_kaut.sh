8f86d53d218b2745e02207014b7676f4381beaaf
buck06191 <buck06191@gmail.com>
2018-05-14 17:01:27 +0100

    Legion code to vary only kaut

---
+++ legion_scripts/PLOS_just_kaut.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#$ -l h_rt=0:20:00
+#$ -N just_kaut_impaired
+#$ -wd /home/ucbpjru/Scratch
+# Set up the job array.  In this instance we have requested 1000 tasks
+# numbered 1 to 1000.
+#$ -t 1-1000
+
+module load python3/recommended
+cd $TMPDIR
+export BASEDIR="$HOME/BayesCMD"
+
+DATAFILE="$BASEDIR/PLOS_paper/data/simulated_smooth_combined_impaired_ABP.csv"
+CONFIGFILE="$BASEDIR/examples/configuration_files/just_kaut.json"
+
+start=`date +%s`
+python3 $BASEDIR/batch_Bayes/batch.py 1000 $DATAFILE $CONFIGFILE --workdir $TMPDIR
+echo "Duration: $(($(date +%s)-$start))" > $TMPDIR/$SGE_TASK_ID.timings.txt
+
+tar -zcvf $HOME/Scratch/batch_$JOB_NAME.$SGE_TASK_ID.tar.gz $TMPDIR
