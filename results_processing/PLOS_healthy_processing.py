857d4c287a38847083b68cd914e678fb5c8ab9da
buck06191 <buck06191@gmail.com>
2018-06-02 14:52:21 +0100

    results processing script updated to use dtw results

--- results_processing/PLOS_healthy_processing.py
+++ results_processing/PLOS_healthy_processing.py
@@ -30,8 +30,8 @@ ap.add_argument(
 
 args = ap.parse_args()
 
-# pfile = data_merge_by_batch(args.parent_dir)
-pfile = os.path.abspath(os.path.join(args.parent_dir, 'all_parameters.csv'))
+pfile = data_merge_by_batch(args.parent_dir)
+# pfile = os.path.abspath(os.path.join(args.parent_dir, 'all_parameters.csv'))
 
 with open(args.conf, 'r') as conf_f:
     conf = json.load(conf_f)
@@ -66,7 +66,7 @@ results = data_import(pfile)
 # Set accepted limit, lim
 lims = [100, 500, 1000]
 distances = []
-for dist_measure in ['NRMSE']:
+for dist_measure in ['DTW']:
     distances.extend(['{}_{}'.format(t, dist_measure)
                       for t in config['targets']])
     distances.append(dist_measure)
@@ -75,7 +75,7 @@ for lim in lims:
         print("Working on {}".format(d.upper()))
         figPath = "/home/buck06191/Dropbox/phd/Bayesian_fitting/{}/{}/{}/{}/{}/"\
             "Figures/{}".format(model_name, 'PLOS_paper',
-                                'Healthy', 'wide_range', 'euclidean_SA', d)
+                                'Healthy', 'narrow_range', 'dtw', d)
 
         dir_util.mkpath(figPath)
         print("Plotting total histogram")
