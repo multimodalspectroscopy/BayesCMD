3bf8af2d78be59cd0dd610971e6a756d9cee534a
buck06191 <buck06191@gmail.com>
2018-07-02 13:00:41 +0900

    changes to code for ISOTT presentation

--- steady_state/steady_state_data_generate.py
+++ steady_state/steady_state_data_generate.py
@@ -57,7 +57,7 @@ cbar = sns.color_palette("muted", n_colors=4)
 
 # outputs = ["CMRO2", "CCO", "DHbT", "CBF", "DHbdiff",
 #            "TOI", "Vmca", "DHbO2", "DHHb", "HHb", "HbO2"]
-outputs = ['TOI', 'CBF', 'Vmca', 'CCO', 'CMRO2']
+outputs = ['TOI', 'DHbdiff', 'CBF', 'CCO', 'CMRO2']
 # For debugging
 # outputs.extend(["k_MAshut", "k_nMAshut", "Q_temp", "_ADP", "_ATP"])
 
@@ -81,8 +81,8 @@ for q in q10_range:
             "Q_10": q
         },
         "targets": outputs,
-        "max_val": 37.5,
-        "min_val": 27.5,
+        "max_val": 37,
+        "min_val": 33.5,
         "debug": False,
         "direction": direction
     }
