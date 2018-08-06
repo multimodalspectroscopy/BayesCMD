8c82517cd88faef3e5a4a7fd4df17d7a9626760e
buck06191 <buck06191@gmail.com>
2018-08-06 13:12:18 +0100

    experimental data legion submission script

---
+++ legion_scripts/PLOS_experimental_hypoxia.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#$ -l h_rt=0:45:00
+#$ -N experimental_hypoxia_wide
+#$ -wd /home/ucbpjru/Scratch
+# Set up the job array.  In this instance we have requested 1000 tasks
+# numbered 1 to 1000.
+#$ -t 1-10000
+
+module load python3/recommended
+cd $TMPDIR
+export BASEDIR="$HOME/BayesCMD"
+
+DATAFILE="$BASEDIR/PLOS_paper/data/cleaned_hypoxia_experimental.csv"
+CONFIGFILE="$BASEDIR/examples/configuration_files/experimental_hypoxia_config.json"
+
+start=`date +%s`
+python3 $BASEDIR/batch_Bayes/batch.py 2000 $DATAFILE $CONFIGFILE --workdir $TMPDIR
+echo "Duration: $(($(date +%s)-$start))" > $TMPDIR/$SGE_TASK_ID.timings.txt
+
+tar -zcvf $HOME/Scratch/batch_$JOB_NAME.$SGE_TASK_ID.tar.gz $TMPDIR
