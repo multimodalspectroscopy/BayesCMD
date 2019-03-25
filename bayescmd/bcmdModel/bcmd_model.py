c6d318c12599cde0cdf932c183a3f6fbbc4a20d9
Josh Buckland <buck06191@gmail.com>
2019-03-25 10:00:02 +0000

    Fixed error with fieldnames in model_selection

--- bayescmd/bcmdModel/bcmd_model.py
+++ bayescmd/bcmdModel/bcmd_model.py
@@ -32,7 +32,7 @@ from .input_creation import InputCreator
 from bayescmd.util import findBaseDir
 
 # default timeout, in seconds
-TIMEOUT = 30
+TIMEOUT = 15
 # default base directory - this should be a relative directory path
 # leading to BayesCMD/
 BASEDIR = os.environ.get('BASEDIR', findBaseDir('BayesCMD'))
